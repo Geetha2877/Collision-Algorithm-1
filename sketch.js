32b72f81a9b4eb28b30e9cb48870cbdb7a9b46ad
Geetha2877 <61772587+Geetha2877@users.noreply.github.com>
2020-04-18 18:46:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,13 @@
 var fixedRect, movingRect;
-var gameObject1, gameObject2, gameObject3, gameObject4;
 
 function setup() {
   createCanvas(1200,800);
   fixedRect = createSprite(600, 400, 50, 80);
   fixedRect.shapeColor = "green";
- 
-  movingRect = createSprite(800, 400,80,30);
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
   movingRect.shapeColor = "green";
-
-  gameObject1 = createSprite(100, 100, 50, 50);
-  gameObject1.shapeColor = "green";
-  gameObject2 = createSprite(200, 100, 50, 50);
-  gameObject2.shapeColor = "green";
-  gameObject3 = createSprite(300, 100, 50, 50);
-  gameObject3.shapeColor = "green";
-  gameObject4 = createSprite(400, 100, 50, 50);
-  gameObject4.shapeColor = "green";
+  movingRect.debug = true;
 }
 
 function draw() {
@@ -24,15 +15,17 @@ function draw() {
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
 
-  if(isTouching(movingRect, gameObject1)){
-    movingRect.shapeColor = "blue";
-    gameObject1.shapeColor = "blue";
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
   }
   else {
     movingRect.shapeColor = "green";
-    gameObject1.shapeColor = "green";
+    fixedRect.shapeColor = "green";
   }
   drawSprites();
 }
-
 
